4ab3eaf1e59f57c3c86c27ef446c3c37fde410fe
sillybop <sillybop@10.0.0.164>
2019-09-16 20:00:57 -0700

    Done.

--- src/OldLady.java
+++ src/OldLady.java
@@ -2,8 +2,42 @@
 public class OldLady {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// Old Lady Rhyme
+		System.out.println("There was an old lady who swallowed a fly.");
+		fly();
+		System.out.println("\nThere was an old lady who swallowed a spider,");
+		System.out.println("That wriggled and iggled and jiggled inside her.");
+		spider();
+		System.out.println("\nThere was an old lady who swallowed a bird,");
+		System.out.println("How absurd to swallow a bird.");
+		bird();
+		System.out.println("\nThere was an old lady who swallowed a cat,");
+		System.out.println("Imagine that to swallow a cat.");
+		cat();
+		System.out.println("\nThere was an old lady who swallowed a dog,");
+		System.out.println("What a hog to swallow a dog.");
+		dog();
+		System.out.println("\nThere was an old lady who swallowed a horse,");
+		System.out.println("She died of course!");
 	}
-
+	public static void fly() {
+		System.out.println("I don't know why she swallowed that fly,");
+		System.out.println("Perhaps she'll die.");
+	}
+	public static void spider() {
+		System.out.println("She swallowed the spider to catch the fly,");
+		fly();
+	}
+	public static void bird() {
+		System.out.println("She swallowed the bird to catch the spider,");
+		spider();
+	}
+	public static void cat() {
+		System.out.println("She swallowed the cat to catch the bird,");
+		bird();
+	}
+	public static void dog() {
+		System.out.println("She swallowed the dog to catch the cat,");
+		cat();
+	}
 }
